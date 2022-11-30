eec777dc0802bb384094f9e35ddb1e962faa70ac
Tricia H <101357697+triciahumann@users.noreply.github.com>
2022-11-30 10:17:52 -0800

    Answered level 1 exercises #1-6

---
+++ day2strings.js
@@ -0,0 +1,21 @@
+// 1. declare a variable named challenge and assign it to an intial value '30 days of js'
+let challenge = '30 Days Of JavaScript';
+
+// 2. Print the string on the browser console using console.log()
+console.log(challenge);
+
+// 3. Print the length of the string on the browser console using console.log()
+console.log(challenge.length);
+
+// 4. Change all the string characters to capital letters using toUpperCase() method
+console.log(challenge.toUpperCase());
+
+// 5. Change all the string characters to lowercase letters using toLowerCase() method
+console.log(challenge.toLowerCase());
+
+// 6. Cut (slice) out the first word of the string using substr() or substring() method
+// substring(): It takes two arguments, the starting index and the stopping index but 
+// it doesn't include the character at the stopping index.
+console.log(challenge.substring(2, 21));
+
+// 7. Slice out the phrase Days Of JavaScript from 30 Days Of JavaScript.

400be540447332ec0d39cbe87c4923b3ea66b9f9
Tricia H <101357697+triciahumann@users.noreply.github.com>
2022-11-30 10:41:42 -0800

    Completed all level 1 exercises

--- day2strings.js
+++ day2strings.js
@@ -38,3 +38,59 @@ console.log(challenge.split(" "))
 
 // 11. 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' 
 // split the string at the comma and change it to an array.
+let faang = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
+console.log(faang.split(" "));
+
+// 12. Change 30 Days Of JavaScript to 30 Days Of Python using replace() method.
+console.log(challenge.replace('JavaScript', 'Python'));
+
+// 13. What is character at index 15 in '30 Days Of JavaScript' string? Use charAt() method.
+console.log(challenge.charAt(15));
+
+// 14. What is the character code of J in '30 Days Of JavaScript' string using charCodeAt()
+console.log(challenge.charCodeAt('J'))
+
+// 15. Use indexOf to determine the position of the first occurrence of a in 30 Days Of JavaScript
+console.log(challenge.indexOf('a'));
+
+// 16. Use lastIndexOf to determine the position of the last occurrence of a in 30 Days Of JavaScript.
+console.log(challenge.lastIndexOf('a'));
+
+/* 17. Use indexOf to find the position of the first occurrence of
+ the word because in the following sentence:'You cannot end a 
+ sentence with because because because is a conjunction' */
+ let sentence = "You cannot end a sentence with because because because is a conjunction";
+ console.log(sentence.indexOf('because'))
+
+ /* 18. Use lastIndexOf to find the position of the last occurrence
+  of the word because in the following sentence:'You cannot end a 
+  sentence with because because because is a conjunction' */
+  console.log(sentence.lastIndexOf('because'));
+
+  /* 19. Use search to find the position of the first occurrence 
+  of the word because in the following sentence:'You cannot end 
+  a sentence with because because because is a conjunction' */
+  console.log(sentence.search('because'));
+
+  /* 20. Use trim() to remove any trailing whitespace at the 
+  beginning and the end of a string.E.g ' 30 Days Of JavaScript '.*/
+  let newChallenge = ' 30 Days of JavaScript ';
+  console.log(newChallenge.trim())
+
+// 21. Use startsWith() method with the string 30 Days Of JavaScript and make the result true
+console.log(challenge.startsWith('30'));
+
+// 22. Use endsWith() method with the string 30 Days Of JavaScript and make the result true
+console.log(challenge.endsWith('t'))
+
+// 23. Use match() method to find all the a’s in 30 Days Of JavaScript
+console.log(challenge.match('a'))
+
+// 24. Use concat() and merge '30 Days of' and 'JavaScript' to a single string, '30 Days Of JavaScript'
+let firstString = '30 Days Of';
+let secondString = " JavaScript";
+let fullString = firstString.concat(secondString);
+console.log(fullString)
+
+// 25. Use repeat() method to print 30 Days Of JavaScript 2 times
+console.log(challenge.repeat(2))

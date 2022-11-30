bb4c4d7f8db8318630cce58d64cdbeac5ada73d4
Tricia H <101357697+triciahumann@users.noreply.github.com>
2022-11-30 14:29:52 -0800

    Finished all but the last question of level 3 exercises

--- day2strings.js
+++ day2strings.js
@@ -186,6 +186,36 @@ console.log(newString);
 
 
 
+// ------- L E V E L   3   E X E R C I S E S
 
+/* 1. 'Love is the best thing in this world. Some found their love and 
+some are still looking for their love.' Count the number of word 
+love in this sentence. */
+let newSentence = 'Love is the best thing in this world. Some found their love and some are still looking for their love.'
+let numOfLove = newSentence.split('love').length;
+console.log(numOfLove) // 3
 
-// L E V E L   3   E X E R C I S E S
+/* 2. Use match() to count the number of all because in the following 
+sentence:'You cannot end a sentence with because because because is a 
+conjunction' */
+let matchBecause = sentence.match('because');
+console.log(matchBecause)
+
+/* 3. Clean the following text and find the most frequent word (hint, use replace and regular expressions).
+    
+    '%I $am@% a %tea@cher%, &and& I lo%#ve 
+    %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as 
+    educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re 
+    interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u 
+    to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the 
+    $resu@lt of &love& of tea&ching'
+*/
+let ridiculousSentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching';
+let cleanSentence = ridiculousSentence.replaceAll('%', "");
+let cleanSentence2 = cleanSentence.replaceAll('$', "")
+let cleanSentence3 = cleanSentence2.replaceAll('@', "");
+let cleanSentence4 = cleanSentence3.replaceAll('#', "");
+let cleanSentence5 = cleanSentence4.replaceAll('&', "");
+
+console.log(cleanSentence5);
+

2db326255f4dc9b0f259c6b76d733a1807d0754d
Tricia H <101357697+triciahumann@users.noreply.github.com>
2022-11-30 10:26:00 -0800

    Answered #7-10 of Level 1 exercises

--- day2strings.js
+++ day2strings.js
@@ -19,3 +19,22 @@ console.log(challenge.toLowerCase());
 console.log(challenge.substring(2, 21));
 
 // 7. Slice out the phrase Days Of JavaScript from 30 Days Of JavaScript.
+console.log(challenge.slice(0,3));
+
+// 8. Check if the string contains a word Script using includes() method
+console.log(challenge.includes('Script'));
+// case sensitive, provided "false" output when I did inludes('script')
+
+// 9. Split the string into an array using split() method
+console.log(challenge.split());
+// ['30 Days Of JavaScript']
+console.log(challenge.split(""));
+// (21) ['3', '0', ' ', 'D'....]
+console.log(challenge.split(" "));
+// (4) ['30', 'Days', 'Of', 'JavaScript']
+
+// 10. Split the string 30 Days Of JavaScript at the space using split() method
+console.log(challenge.split(" "))
+
+// 11. 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' 
+// split the string at the comma and change it to an array.

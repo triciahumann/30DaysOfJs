f138826c89ddaf07ba1c5c8800445f08cb3dbbd0
Tricia H <101357697+triciahumann@users.noreply.github.com>
2022-11-30 09:07:01 -0800

    Initial commit

---
+++ datatype.js
@@ -0,0 +1,39 @@
+/* use the 
+JavaScript typeof operator to check different 
+data types. Check the data type of each variable */
+console.log(typeof false);
+console.log(typeof undefined);
+console.log(typeof null);
+
+/* Declare four variables without assigning values
+Declare four variables with assigned values */
+
+// const ageToday = 10;
+// const lastName = "benedict";
+// const isMarried = true;
+// const humanDesign = "projector";
+
+/* Declare variables to store your first name, last name, 
+marital status, country and age in multiple lines */
+// let firstName = "tricia";
+// const lastName= "humann";
+// const isMarried = true;
+// const country = "usa";
+// const myAge = 30;
+
+/*Declare variables to store your first name, last name,
+marital status, country and age in a single line */
+
+let firstName = "john",
+    lastName = "benedict"
+
+
+
+
+// Declare two variables myAge and yourAge and 
+//assign them initial values and log to the browser console.
+
+const yourAge = 20;
+const hisAge = 33;
+
+console.log("I am", yourAge, "and he is", hisAge)
---
+++ introduction.js
@@ -0,0 +1,11 @@
+let age = 10;
+//const firstName = "Tricia";
+
+//console.log(firstName);
+
+// comments can make code readable
+// Welcome to 30Daysofjs
+
+/* comments can 
+make code readable, and 
+easy to use */
